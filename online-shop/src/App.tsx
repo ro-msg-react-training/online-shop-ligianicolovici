68989eb5fb9dd102e2cf33735502a59c9b739dbd
Ligia Nicolovici <Ligia.Nicolovici@msg.group>
2019-10-11 17:59:59 +0300

    refactor in code after the code review + chapter 6 done except for the checkOut task

--- online-shop/src/App.tsx
+++ online-shop/src/App.tsx
@@ -1,67 +1,85 @@
-import React from 'react';
-import './App.css';
-import './productModeling/product.css';
-import './productModeling/productList.css';
-import PostData from './products.json';
-import ProductList from './components/ProductList';
-import ProductDetails from './components/ProductDetails';
-import './App.sass';
-import {  Switch, Route } from 'react-router';
-import {BrowserRouter as Router } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import ShoppingCart from './components/ShoppingCart';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStroopwafel } from '@fortawesome/free-solid-svg-icons';
-library.add(faStroopwafel)
+import React from "react";
+import "./App.css";
+import "./productModeling/product.css";
+import "./productModeling/productList.css";
+import PostData from "./products.json";
+import ProductList from "./components/ProductList";
+import ProductDetails from "./components/ProductDetails";
+import "./App.sass";
+import { Switch, Route, Redirect } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import ShoppingCart from "./components/ShoppingCart";
+import { IPropsApp, IStateApp, IProduct } from "./model/Interfaces";
 
-export interface IProduct{
-  id:number;
-  name:string;
-  category:string;
-  image:string;
-  price:number;
-  description:string;
-}
-
-interface IProps {
-  data?: any
-}
-interface IState{
-  itemsCart:IProduct[];
-}
-export default class App extends React.Component<IProps,IState>{
-   products: IProduct[] = Object.values(PostData);
-  constructor(props:IProps){
-    super(props);
-    this.state={
-      itemsCart:[]
-    }
+export default class App extends React.Component<IPropsApp, IStateApp> {
+  componentDidMount() {
+    fetch("http://localhost:4000/products")
+      .then(response => response.json())
+      .then(data => this.setState({ products: data }));
   }
-  changeStateArray(cartProducts:IProduct[]){
+
+  addProductsToCart(newCartProduct: IProduct) {
     this.setState({
-        itemsCart:this.state.itemsCart.concat(cartProducts)
+      itemsCart: this.state.itemsCart.concat(newCartProduct)
     });
   }
- 
-  render(){
-  return (
-    <Router>
-          <div className="App"> 
-          <NavBar></NavBar>
-              <Switch>
-                  <Route path="/" exact render= {()=> <ProductList data={this.products}/>}/>
-                  <Route path="/products" exact render= {()=> <ProductList data={this.products}/>}/>
-                  {/* <Route path="/products/:id" exact render={() =><ProductDetails/> }/> */}
-                  <Route path="/products/:id" exact render={(props) =><ProductDetails {...props} appSetArray={this.changeStateArray.bind(this)}/> }/>
-                  <Route path="/cart" exact render={()=> <ShoppingCart data= {this.state.itemsCart}/>}/>
-              </Switch>
-            </div>
-   </Router>
+  deleteProduct(productID: number) {
+    return fetch("http://localhost:4000/products/" + productID, {
+      method: "delete"
+    })
+      .then(response => response.text())
+      .then(() => {
+        fetch("http://localhost:4000/products")
+          .then(response => response.json())
+          .then(data => this.setState({ products: data }));
+      })
+      .then(() => <Redirect to="/products"></Redirect>);
+  }
 
-    
+  constructor(props: IPropsApp) {
+    super(props);
+    this.state = {
+      itemsCart: [],
+      products: []
+    };
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <NavBar></NavBar>
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={() => <ProductList data={this.state.products} />}
+            />
+            <Route
+              path="/products"
+              exact
+              render={() => <ProductList data={this.state.products} />}
+            />
+            <Route
+              path="/products/:id"
+              exact
+              render={props => (
+                <ProductDetails
+                  {...props}
+                  onAddProduct={this.addProductsToCart.bind(this)}
+                  onDeleteProduct={this.deleteProduct.bind(this)}
+                />
+              )}
+            />
+            <Route
+              path="/cart"
+              exact
+              render={() => <ShoppingCart data={this.state.itemsCart} />}
+            />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
-
-
--- online-shop/src/components/ShoppingCart.tsx
+++ online-shop/src/components/ShoppingCart.tsx
@@ -1,68 +1,50 @@
-import React from 'react'
-import '../productModeling/shoppingcart.css';
-import { IProduct } from '../App.js';
+import React from "react";
+import "../productModeling/shoppingcart.css";
+import { IPropsCart, IStateCart } from "../model/Interfaces";
 
+export class ShoppingCart extends React.Component<IPropsCart, IStateCart> {
+  constructor(props: IPropsCart) {
+    super(props);
+    this.state = {
+      cartProducts: this.props.data
+    };
+  }
+  render() {
+    let productsFromCart;
+    if (this.state.cartProducts != null) {
+      productsFromCart = this.state.cartProducts.map((productt, key) => (
+        <tr>
+          <td>{productt.name}</td>
+          <td>{productt.category}</td>
+          <td>{productt.price}</td>
+        </tr>
+      ));
+    } else
+      productsFromCart = (
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      );
 
-
-interface IProps {
-    data: IProduct[];
- 
-}
-interface IState{
-    cartProducts :IProduct[];
-}
-
-
-export class ShoppingCart extends React.Component<IProps,IState>{
-    productsFromCart:any;
-    constructor(props:IProps){
-        super(props);
-        this.state={
-            cartProducts:this.props.data
-        }
-    
-        
-    }
-    render() {
-        if(this.state.cartProducts!=null){
-              this.productsFromCart= this.state.cartProducts.map(
-                (productt) =>   
-                            <tr>
-                                <td>{productt.name}</td>
-                                <td>{productt.category}</td>
-                                <td>{productt.price}</td>
-                            </tr>         
-            );        
-        }else(
-             this.productsFromCart=(
-                <tr>
-                    <td>null</td>
-                    <td>null</td>
-                    <td>null</td>
-                </tr>         
-             )
-        );
-        
-        return (
-            <div className='content'>
-                <h1 className="subtitle is-2">My shopping cart</h1>
-                <div className='cart-list' >
-                        <table className="table ">
-                        <thead>
-                            <tr>
-                                <th>Name </th>
-                                <th>Category </th>
-                                <th>Price </th>
-                                
-                            </tr>
-                        </thead>
-                            <tbody>
-                            {this.productsFromCart}
-                            </tbody>
-                            </table>
-                       </div>
-            </div>
-        );
-    }
+    return (
+      <div className="content">
+        <h1 className="subtitle is-2">My shopping cart</h1>
+        <div className="cart-list">
+          <table className="table ">
+            <thead>
+              <tr>
+                <th>Name </th>
+                <th>Category </th>
+                <th>Price </th>
+              </tr>
+            </thead>
+            <tbody>{productsFromCart}</tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 export default ShoppingCart;
